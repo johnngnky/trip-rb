5782a815b7ca154ffc38f7c8cc34e2d5fee13693
Jurgen van den Brink <jurgen@solutior.nl>
2019-01-16 13:51:27 +0100

    Implement Bootstrap-responsiveness

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -27,94 +27,41 @@ export class Header extends React.PureComponent<{
             <>
                 <div className="container-fluid">
                     <div className="row">
-                        <div className="col-6 d-flex align-items-center">
+                        <div className="col-9 col-lg-6 d-flex align-items-center">
+                            <button
+                                type="button"
+                                className="btn btn-secondary mr-3 d-lg-none"
+                                title="Open the editor"
+                                id="editor_button"
+                                onClick={() => this.toggleEditor()}
+                            >
+                                Edit
+                            </button>
                             <h1 className="mr-1 mr-md-3 text-truncate" onClick={() => this.props.editor.edit()}>
                                 {this.blocks.name || "Unnamed"}
                             </h1>
-                            <a
-                                href="https://gitlab.com/tripetto/examples/react"
-                                target="_blank"
-                                role="button"
-                                className="btn btn-sm btn-link"
-                            >
-                                <i className="fas fa-code pr-2" />
-                                Get source
-                            </a>
-                            <a
-                                href="https://gitlab.com/tripetto/examples/react/blob/master/README.md"
-                                target="_blank"
-                                role="button"
-                                className="btn btn-sm btn-link"
-                            >
-                                <i className="fas fa-book-reader pr-2" />
-                                View readme
-                            </a>
-                            <a href="#" onClick={() => this.props.reset()} role="button" className="btn btn-sm btn-link">
-                                <i className="fas fa-redo pr-2" />
-                                Reset demo
-                            </a>
+                            <div className="d-none d-lg-block text-nowrap">{this.renderButtonsDocs(false)}</div>
                         </div>
-                        <div className="col-6 d-flex justify-content-end align-items-center">
-                            {!this.blocks.preview && (
-                                <>
-                                    <div className="btn-group mr-1 mr-md-4" role="group" aria-label="Controls">
-                                        <button
-                                            type="button"
-                                            className="btn btn-primary"
-                                            title="Start form"
-                                            disabled={this.blocks.isEmpty || (!this.blocks.isStopped && !this.blocks.isFinished)}
-                                            onClick={() => this.collector.start()}
-                                        >
-                                            <i className="fas fa-play fa-fw" />
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="btn btn-primary"
-                                            title="Pause form"
-                                            disabled={!this.blocks.isRunning}
-                                            onClick={() => this.collector.pause()}
-                                        >
-                                            <i className="fas fa-pause fa-fw" />
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="btn btn-primary"
-                                            title="Stop form"
-                                            disabled={!this.blocks.isRunning}
-                                            onClick={() => this.collector.stop()}
-                                        >
-                                            <i className="fas fa-stop fa-fw" />
-                                        </button>
-                                    </div>
-                                    <button
-                                        type="button"
-                                        className="btn btn-primary mr-1 mr-md-4"
-                                        data-toggle="modal"
-                                        data-target="#settingsModal"
-                                    >
-                                        Settings
-                                    </button>
-                                </>
-                            )}
-                            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                        <div className="col-3 col-lg-6 d-flex justify-content-end align-items-center">
+                            <div className="d-none d-lg-block">{this.renderButtonsControls(false)}</div>
+                            <div className="d-lg-none">
                                 <button
+                                    className="btn btn-secondary dropdown-toggle"
                                     type="button"
-                                    className={`btn ${!this.blocks.preview ? "btn-success" : "btn-secondary"}`}
-                                    data-toggle="button"
-                                    aria-pressed={!this.blocks.preview}
-                                    onClick={() => (this.blocks.preview = false)}
+                                    id="dropdownMenu"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true"
+                                    aria-expanded="false"
                                 >
-                                    Collect
+                                    <i className="fas fa-cog fa-fw mr-1" />
                                 </button>
-                                <button
-                                    type="button"
-                                    className={`btn ${this.blocks.preview ? "btn-success" : "btn-secondary"}`}
-                                    data-toggle="button"
-                                    aria-pressed={this.blocks.preview}
-                                    onClick={() => (this.blocks.preview = true)}
-                                >
-                                    Preview
-                                </button>
+                                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu">
+                                    <h6 className="dropdown-header">Demo documentation</h6>
+                                    {this.renderButtonsDocs(true)}
+                                    <div className="dropdown-divider" />
+                                    <h6 className="dropdown-header">Demo controls</h6>
+                                    {this.renderButtonsControls(true)}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -123,5 +70,127 @@ export class Header extends React.PureComponent<{
                 {settingsModal(this.collector)}
             </>
         );
+    }
+
+    public renderButtonsDocs(dropdown: boolean): React.ReactNode {
+        return (
+            <>
+                <a
+                    href="https://gitlab.com/tripetto/examples/react"
+                    target="_blank"
+                    role="button"
+                    className={`btn btn-sm btn-link${dropdown ? " dropdown-item" : ""}`}
+                >
+                    <i className="fas fa-code fa-fw mr-2" />
+                    Get source
+                </a>
+                <a
+                    href="https://gitlab.com/tripetto/examples/react/blob/master/README.md"
+                    target="_blank"
+                    role="button"
+                    className={`btn btn-sm btn-link${dropdown ? " dropdown-item" : ""}`}
+                >
+                    <i className="fas fa-book-reader fa-fw mr-2" />
+                    View readme
+                </a>
+                <a
+                    href="#"
+                    onClick={() => this.props.reset()}
+                    role="button"
+                    className={`btn btn-sm btn-link${dropdown ? " dropdown-item" : ""}`}
+                >
+                    <i className="fas fa-redo fa-fw mr-2" />
+                    Reset demo
+                </a>
+            </>
+        );
+    }
+
+    public renderButtonsControls(dropdown: boolean): React.ReactNode {
+        return (
+            <>
+                {!this.blocks.preview && (
+                    <>
+                        <div
+                            className={`btn-group controls ${dropdown ? "px-4 py-2 offset-down" : "mr-1 mr-sm-4"}`}
+                            role="group"
+                            aria-label="Controls"
+                        >
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                title="Start form"
+                                disabled={this.blocks.isEmpty || (!this.blocks.isStopped && !this.blocks.isFinished)}
+                                onClick={() => this.collector.start()}
+                            >
+                                <i className="fas fa-play fa-fw" />
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                title="Pause form"
+                                disabled={!this.blocks.isRunning}
+                                onClick={() => this.collector.pause()}
+                            >
+                                <i className="fas fa-pause fa-fw" />
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                title="Stop form"
+                                disabled={!this.blocks.isRunning}
+                                onClick={() => this.collector.stop()}
+                            >
+                                <i className="fas fa-stop fa-fw" />
+                            </button>
+                        </div>
+                        <button
+                            type="button"
+                            className={`btn ${dropdown ? "btn-link btn-sm dropdown-item offset-up" : "btn-primary mr-1 mr-md-4"}`}
+                            data-toggle="modal"
+                            data-target="#settingsModal"
+                        >
+                            <i className="fas fa-cog fa-fw mr-2 d-lg-none" />
+                            Settings
+                        </button>
+                        <div className="dropdown-divider offset-up d-lg-none" />
+                    </>
+                )}
+                <div className={`btn-group btn-group-toggle${dropdown ? " px-4 py-2" : ""}`} data-toggle="buttons">
+                    <button
+                        type="button"
+                        className={`btn ${!this.blocks.preview ? "btn-success" : "btn-secondary"}`}
+                        data-toggle="button"
+                        aria-pressed={!this.blocks.preview}
+                        onClick={() => (this.blocks.preview = false)}
+                    >
+                        Collect
+                    </button>
+                    <button
+                        type="button"
+                        className={`btn ${this.blocks.preview ? "btn-success" : "btn-secondary"}`}
+                        data-toggle="button"
+                        aria-pressed={this.blocks.preview}
+                        onClick={() => (this.blocks.preview = true)}
+                    >
+                        Preview
+                    </button>
+                </div>
+            </>
+        );
+    }
+
+    /** Toggles the editor to show/hide. */
+    toggleEditor(): void {
+        const editor = document.getElementById("editor");
+        const editor_button = document.getElementById("editor_button");
+
+        if (editor && editor_button) {
+            editor.classList.toggle("show");
+            editor_button.classList.toggle("btn-success");
+            editor_button.classList.toggle("btn-secondary");
+            editor_button.title = editor_button.classList.contains("btn-success") ? "Close the editor" : "Open the editor";
+            setTimeout(() => this.props.editor.resize(), 250);
+        }
     }
 }
